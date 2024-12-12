b1fb062353c8bd71591247fa38a9c4024a1f51b7
Mehrshad Fallah Estalkhzir <fallah_mehrshad82@comp.iust.ac.ir>
2024-12-12 07:27:00 +0330

    feat/model training functions added. More testing needed

--- datasets/items_dataset_creator.py
+++ datasets/items_dataset_creator.py
@@ -2,10 +2,18 @@ import pandas as pd
 import random
 
 # تعداد کالاها
-num_items = 1400
+num_items = 450
+
+# توزیع‌های نامتوازن‌تر برای ویژگی‌ها
+category_weights = [0.37, 0.12, 0.06, 0.11, 0.04, 0.07, 0.04, 0.14]
+color_weights = [0.3, 0.1, 0.05, 0.15, 0.05, 0.2, 0.15]
+material_weights = [0.35, 0.1, 0.05, 0.2, 0.05, 0.1, 0.15]
+design_style_weights = [0.4, 0.15, 0.05, 0.1, 0.1, 0.1, 0.1]
+usage_weights = [0.5, 0.1, 0.05, 0.1, 0.05, 0.1, 0.1]
+gender_weights = [0.6, 0.3, 0.1]
 
 # ویژگی‌های کالا
-categories = ["الکترونیک", "پوشاک و اکسسوری", "خانه و دکور", "زیبایی و بهداشت", "ورزش و تناسب اندام", "کتاب و محصولات آموزشی","پازل‌ها و بازی‌ها","لوازم سفر و گردشگری"]
+categories = ["الکترونیک", "پوشاک و اکسسوری", "خانه و دکور", "زیبایی و بهداشت", "ورزش و تناسب اندام", "کتاب و محصولات آموزشی", "پازل‌ها و بازی‌ها", "لوازم سفر و گردشگری"]
 colors = ["خنثی", "اصلی", "ثانویه", "طبیعی", "روشن و شاد", "فلزی", "تیره و لوکس"]
 materials = ["پارچه", "فلزات", "پلاستیک", "چوبی", "شیشه و سرامیک", "ترکیبی", "خاص"]
 design_styles = ["کلاسیک", "مدرن", "هنری", "لوکس و رسمی", "طبیعت‌محور", "منطقه‌ای", "فانتزی و خاص"]
@@ -15,12 +23,12 @@ genders = ["مردانه", "زنانه", "خنثی"]
 # ایجاد دیتاست
 items_data = {
     "شناسه کالا": list(range(1, num_items + 1)),
-    "دسته‌بندی": [random.choice(categories) for _ in range(num_items)],
-    "رنگ": [random.choice(colors) for _ in range(num_items)],
-    "متریال": [random.choice(materials) for _ in range(num_items)],
-    "سبک طراحی": [random.choice(design_styles) for _ in range(num_items)],
-    "کاربرد": [random.choice(usages) for _ in range(num_items)],
-    "جنسیت": [random.choice(genders) for _ in range(num_items)],
+    "دسته‌بندی": random.choices(categories, weights=category_weights, k=num_items),
+    "رنگ": random.choices(colors, weights=color_weights, k=num_items),
+    "متریال": random.choices(materials, weights=material_weights, k=num_items),
+    "سبک طراحی": random.choices(design_styles, weights=design_style_weights, k=num_items),
+    "کاربرد": random.choices(usages, weights=usage_weights, k=num_items),
+    "جنسیت": random.choices(genders, weights=gender_weights, k=num_items),
 }
 
 # تبدیل به DataFrame
